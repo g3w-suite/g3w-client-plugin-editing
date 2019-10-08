86366d9da0ef7218038a97e0db39013154385adc
volterra79 <francescoboccacci@libero.it>
2019-10-08 17:25:38 +0200

    changed var to const variable in pckfeaturetask

--- workflows/steps/tasks/pickfeaturetask.js
+++ workflows/steps/tasks/pickfeaturetask.js
@@ -1,9 +1,9 @@
-var inherit = g3wsdk.core.utils.inherit;
-var base =  g3wsdk.core.utils.base;
-var PickFeatureInteraction = g3wsdk.ol.interactions.PickFeatureInteraction;
-var EditingTask = require('./editingtask');
+const inherit = g3wsdk.core.utils.inherit;
+const base =  g3wsdk.core.utils.base;
+const PickFeatureInteraction = g3wsdk.ol.interactions.PickFeatureInteraction;
+const EditingTask = require('./editingtask');
 
-function PickFeatureTask(options) {
+function PickFeatureTask(options={}) {
   this.pickFeatureInteraction = null;
   this._busy = false;
   this._tools = options.tools || [];
@@ -17,20 +17,20 @@ var proto = PickFeatureTask.prototype;
 // metodo eseguito all'avvio del tool
 proto.run = function(inputs, context) {
   //console.log('Pick Feature Task run ....');
-  var d = $.Deferred();
+  const d = $.Deferred();
   //var style = this.editor._editingVectorStyle ? this.editor._editingVectorStyle.edit : null;
   // vado a settare i layers su cui faccio l'interacion agisce
-  var layers = [inputs.layer];
-  var features = inputs.features.length ? inputs.features : null;
+  const layers = [inputs.layer];
+  const features = inputs.features.length ? inputs.features : null;
   this.pickFeatureInteraction = new PickFeatureInteraction({
-    layers: layers,
-    features: features
+    layers,
+    features
   });
   // aggiungo
   this.addInteraction(this.pickFeatureInteraction);
   // gestisco l'evento
   this.pickFeatureInteraction.on('picked', function(e) {
-    var feature = e.feature;
+    const feature = e.feature;
     if (!features)
       inputs.features.push(feature);
     d.resolve(inputs);
