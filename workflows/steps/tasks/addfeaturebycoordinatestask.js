332d5262b30fd527b6d25604706b0b15b1d3d6c5
volterra79 <boccacci.francesco@gmail.com>
2021-05-28 17:33:24 +0200

    added all geaomterie supported by qgis

--- workflows/steps/tasks/addfeaturebycoordinatestask.js
+++ workflows/steps/tasks/addfeaturebycoordinatestask.js
@@ -19,13 +19,53 @@ proto.run = function(inputs, context) {
   const editingLayer = originalLayer.getEditingLayer();
   const session = context.session;
   const layerId = originalLayer.getId();
-  let geometryType;
-  if (originalLayer.getEditingGeometryType() === Geometry.GeometryTypes.LINE) geometryType = 'LineString';
-  else if (originalLayer.getEditingGeometryType() === Geometry.GeometryTypes.MULTILINE) geometryType = 'MultiLineString';
-  else geometryType = originalLayer.getEditingGeometryType();
+  let geometryType = originalLayer.getEditingGeometryType();
   switch (geometryType) {
-    case Geometry.GeometryTypes.POINT:
-    case Geometry.GeometryTypes.MULTI_POINT:
+    case Geometry.GeometryTypes.LINESTRINGZ:
+    case Geometry.GeometryTypes.LINESTRINGM:
+    case Geometry.GeometryTypes.LINESTRINGZM:
+    case Geometry.GeometryTypes.LINESTRING25D:
+    case Geometry.GeometryTypes.LINE:
+    case Geometry.GeometryTypes.LINEZ:
+    case Geometry.GeometryTypes.LINEM:
+    case Geometry.GeometryTypes.LINEZM:
+    case Geometry.GeometryTypes.LINE25D:
+      geometryType = 'LineString';
+      break;
+    case Geometry.GeometryTypes.MULTILINESTRINGZ:
+    case Geometry.GeometryTypes.MULTILINESTRINGM:
+    case Geometry.GeometryTypes.MULTILINESTRINGZM:
+    case Geometry.GeometryTypes.MULTILINESTRING25D:
+    case Geometry.GeometryTypes.MULTILINE:
+    case Geometry.GeometryTypes.MULTILINEZ:
+    case Geometry.GeometryTypes.MULTILINEM:
+    case Geometry.GeometryTypes.MULTILINEZM:
+    case Geometry.GeometryTypes.MULTILINE25D:
+      geometryType = 'MultiLineString';
+      break;
+    case Geometry.GeometryTypes.POINTZ:
+    case Geometry.GeometryTypes.POINTM:
+    case Geometry.GeometryTypes.POINTZM:
+    case Geometry.GeometryTypes.POINT25D:
+      geometryType = 'Point';
+      break;
+    case Geometry.GeometryTypes.MULTIPOINTZ:
+    case Geometry.GeometryTypes.MULTIPOINTM:
+    case Geometry.GeometryTypes.MULTIPOINTZM:
+    case Geometry.GeometryTypes.MULTIPOINT25D:
+      geometryType = 'MultiPoint';
+      break;
+    case Geometry.GeometryTypes.POLYGONZ:
+    case Geometry.GeometryTypes.POLYGONM:
+    case Geometry.GeometryTypes.POLYGONZM:
+    case Geometry.GeometryTypes.POLYGON25D:
+      geometryType = 'Polygon';
+      break;
+    case Geometry.GeometryTypes.MULTIPOLYGONZ:
+    case Geometry.GeometryTypes.MULTIPOLYGONM:
+    case Geometry.GeometryTypes.MULTIPOLYGONZM:
+    case Geometry.GeometryTypes.MULTIPOLYGON25D:
+      geometryType = 'MultiPolygon';
       break;
   }
   const source = editingLayer.getSource();
