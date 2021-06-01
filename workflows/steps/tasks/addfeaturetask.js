1ae9fa75f0875269fc87d06785d59b697d6c27a9
volterra79 <boccacci.francesco@gmail.com>
2021-06-01 11:30:02 +0200

    handle add feature with strange 25d z m and other tipologies of geoemtries

--- workflows/steps/tasks/addfeaturetask.js
+++ workflows/steps/tasks/addfeaturetask.js
@@ -28,7 +28,8 @@ proto.run = function(inputs, context) {
   const layerId = originalLayer.getId();
   switch (originalLayer.getType()) {
     case Layer.LayerTypes.VECTOR:
-      let geometryType = originalLayer.getEditingGeometryType();
+      const originalGeometryType = originalLayer.getEditingGeometryType();
+      let geometryType = originalGeometryType;
       switch (geometryType) {
         case Geometry.GeometryTypes.LINESTRINGZ:
         case Geometry.GeometryTypes.LINESTRINGM:
@@ -89,7 +90,7 @@ proto.run = function(inputs, context) {
       });
       this.addInteraction(this.drawInteraction);
       this.drawInteraction.setActive(true);
-      this.drawInteraction.on('drawend', (e) => {
+      this.drawInteraction.on('drawend', e => {
         let feature;
         if (this._add) {
           attributes.forEach(attribute => {
@@ -102,6 +103,74 @@ proto.run = function(inputs, context) {
           source.addFeature(feature);
           session.pushAdd(layerId, feature);
         } else feature = e.feature;
+        const originalFeatureCoordinates = feature.getGeometry().getCoordinates();
+        switch (originalGeometryType){
+          //POINT //[x,y]
+          case Geometry.GeometryTypes.POINTZ:
+          case Geometry.GeometryTypes.POINTM:
+          case Geometry.GeometryTypes.POINTZM:
+          case Geometry.GeometryTypes.POINT25D:
+            originalFeatureCoordinates.push(0);
+            feature.getGeometry().setCoordinates(originalFeatureCoordinates);
+            break;
+          //MULTIPOINT [[x1, y1], [x2, y2]]
+          case Geometry.GeometryTypes.MULTIPOINTZ:
+          case Geometry.GeometryTypes.MULTIPOINTM:
+          case Geometry.GeometryTypes.MULTIPOINTZM:
+          case Geometry.GeometryTypes.MULTIPOINT25D:
+          //LINE [[x1, y1], [x2, y2]]
+          case Geometry.GeometryTypes.LINESTRINGZ:
+          case Geometry.GeometryTypes.LINESTRINGM:
+          case Geometry.GeometryTypes.LINESTRINGZM:
+          case Geometry.GeometryTypes.LINESTRING25D:
+          case Geometry.GeometryTypes.LINEZ:
+          case Geometry.GeometryTypes.LINEM:
+          case Geometry.GeometryTypes.LINEZM:
+          case Geometry.GeometryTypes.LINE25D:
+            originalFeatureCoordinates.forEach(coordinates => coordinates.push(0));
+            feature.getGeometry().setCoordinates(originalFeatureCoordinates);
+            break;
+          //MULTILINE [
+          // [[x1, y1],[x2, y2]],
+          // [[x3, y3], [x4, y4]]
+          // ]
+          case Geometry.GeometryTypes.MULTILINESTRINGZ:
+          case Geometry.GeometryTypes.MULTILINESTRINGM:
+          case Geometry.GeometryTypes.MULTILINESTRINGZM:
+          case Geometry.GeometryTypes.MULTILINESTRING25D:
+          case Geometry.GeometryTypes.MULTILINEZ:
+          case Geometry.GeometryTypes.MULTILINEM:
+          case Geometry.GeometryTypes.MULTILINEZM:
+          case Geometry.GeometryTypes.MULTILINE25D:
+            originalFeatureCoordinates.forEach(singleLine => {
+              singleLine.forEach(coordinates => coordinates.push(0))
+            });
+            feature.getGeometry().setCoordinates(originalFeatureCoordinates);
+            break;
+          //POLYGON [
+          //[[x1, y1], [x2, y2], [x3, y3], [x1, y1]]
+          // ]
+          case Geometry.GeometryTypes.POLYGONZ:
+          case Geometry.GeometryTypes.POLYGONM:
+          case Geometry.GeometryTypes.POLYGONZM:
+          case Geometry.GeometryTypes.POLYGON25D:
+            originalFeatureCoordinates[0].forEach(coordinates => coordinates.push(0));
+            feature.getGeometry().setCoordinates(originalFeatureCoordinates);
+            break;
+          //MULTIPOLYGON  [
+          //       [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ],
+          //       [ [100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2] ]
+          //       ]
+          case Geometry.GeometryTypes.MULTIPOLYGONZ:
+          case Geometry.GeometryTypes.MULTIPOLYGONM:
+          case Geometry.GeometryTypes.MULTIPOLYGOZM:
+          case Geometry.GeometryTypes.MULTIPOLYGON25D:
+            originalFeatureCoordinates.forEach(singlePolygon => {
+              singlePolygon[0].forEach(coordinates => coordinates.push(0))
+            });
+            feature.getGeometry().setCoordinates(originalFeatureCoordinates);
+            break;
+        }
         inputs.features.push(feature);
         d.resolve(inputs);
       });
