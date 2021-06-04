b467ca6af62168fee6264c50380a33b9af8549ca
volterra79 <boccacci.francesco@gmail.com>
2021-06-04 16:08:37 +0200

    reafctor query builder and geoemtry

--- workflows/steps/tasks/addfeaturebycoordinatestask.js
+++ workflows/steps/tasks/addfeaturebycoordinatestask.js
@@ -1,6 +1,4 @@
 const inherit = g3wsdk.core.utils.inherit;
-const Layer = g3wsdk.core.layer.Layer;
-const Geometry = g3wsdk.core.geometry.Geometry;
 const base =  g3wsdk.core.utils.base;
 const EditingTask = require('./editingtask');
 const Feature = g3wsdk.core.layer.features.Feature;
@@ -19,55 +17,6 @@ proto.run = function(inputs, context) {
   const editingLayer = originalLayer.getEditingLayer();
   const session = context.session;
   const layerId = originalLayer.getId();
-  let geometryType = originalLayer.getEditingGeometryType();
-  switch (geometryType) {
-    case Geometry.GeometryTypes.LINESTRINGZ:
-    case Geometry.GeometryTypes.LINESTRINGM:
-    case Geometry.GeometryTypes.LINESTRINGZM:
-    case Geometry.GeometryTypes.LINESTRING25D:
-    case Geometry.GeometryTypes.LINE:
-    case Geometry.GeometryTypes.LINEZ:
-    case Geometry.GeometryTypes.LINEM:
-    case Geometry.GeometryTypes.LINEZM:
-    case Geometry.GeometryTypes.LINE25D:
-      geometryType = 'LineString';
-      break;
-    case Geometry.GeometryTypes.MULTILINESTRINGZ:
-    case Geometry.GeometryTypes.MULTILINESTRINGM:
-    case Geometry.GeometryTypes.MULTILINESTRINGZM:
-    case Geometry.GeometryTypes.MULTILINESTRING25D:
-    case Geometry.GeometryTypes.MULTILINE:
-    case Geometry.GeometryTypes.MULTILINEZ:
-    case Geometry.GeometryTypes.MULTILINEM:
-    case Geometry.GeometryTypes.MULTILINEZM:
-    case Geometry.GeometryTypes.MULTILINE25D:
-      geometryType = 'MultiLineString';
-      break;
-    case Geometry.GeometryTypes.POINTZ:
-    case Geometry.GeometryTypes.POINTM:
-    case Geometry.GeometryTypes.POINTZM:
-    case Geometry.GeometryTypes.POINT25D:
-      geometryType = 'Point';
-      break;
-    case Geometry.GeometryTypes.MULTIPOINTZ:
-    case Geometry.GeometryTypes.MULTIPOINTM:
-    case Geometry.GeometryTypes.MULTIPOINTZM:
-    case Geometry.GeometryTypes.MULTIPOINT25D:
-      geometryType = 'MultiPoint';
-      break;
-    case Geometry.GeometryTypes.POLYGONZ:
-    case Geometry.GeometryTypes.POLYGONM:
-    case Geometry.GeometryTypes.POLYGONZM:
-    case Geometry.GeometryTypes.POLYGON25D:
-      geometryType = 'Polygon';
-      break;
-    case Geometry.GeometryTypes.MULTIPOLYGONZ:
-    case Geometry.GeometryTypes.MULTIPOLYGONM:
-    case Geometry.GeometryTypes.MULTIPOLYGONZM:
-    case Geometry.GeometryTypes.MULTIPOLYGON25D:
-      geometryType = 'MultiPolygon';
-      break;
-  }
   const source = editingLayer.getSource();
   const feature = new Feature({
     feature: e.feature,
--- workflows/steps/tasks/addfeaturetask.js
+++ workflows/steps/tasks/addfeaturetask.js
@@ -1,7 +1,6 @@
-const inherit = g3wsdk.core.utils.inherit;
+const {base, inherit} =  g3wsdk.core.utils;
 const Layer = g3wsdk.core.layer.Layer;
 const Geometry = g3wsdk.core.geometry.Geometry;
-const base =  g3wsdk.core.utils.base;
 const EditingTask = require('./editingtask');
 const Feature = g3wsdk.core.layer.features.Feature;
 
@@ -29,55 +28,7 @@ proto.run = function(inputs, context) {
   switch (originalLayer.getType()) {
     case Layer.LayerTypes.VECTOR:
       const originalGeometryType = originalLayer.getEditingGeometryType();
-      let geometryType = originalGeometryType;
-      switch (geometryType) {
-        case Geometry.GeometryTypes.LINESTRINGZ:
-        case Geometry.GeometryTypes.LINESTRINGM:
-        case Geometry.GeometryTypes.LINESTRINGZM:
-        case Geometry.GeometryTypes.LINESTRING25D:
-        case Geometry.GeometryTypes.LINE:
-        case Geometry.GeometryTypes.LINEZ:
-        case Geometry.GeometryTypes.LINEM:
-        case Geometry.GeometryTypes.LINEZM:
-        case Geometry.GeometryTypes.LINE25D:
-          geometryType = 'LineString';
-          break;
-        case Geometry.GeometryTypes.MULTILINESTRINGZ:
-        case Geometry.GeometryTypes.MULTILINESTRINGM:
-        case Geometry.GeometryTypes.MULTILINESTRINGZM:
-        case Geometry.GeometryTypes.MULTILINESTRING25D:
-        case Geometry.GeometryTypes.MULTILINE:
-        case Geometry.GeometryTypes.MULTILINEZ:
-        case Geometry.GeometryTypes.MULTILINEM:
-        case Geometry.GeometryTypes.MULTILINEZM:
-        case Geometry.GeometryTypes.MULTILINE25D:
-          geometryType = 'MultiLineString';
-          break;
-        case Geometry.GeometryTypes.POINTZ:
-        case Geometry.GeometryTypes.POINTM:
-        case Geometry.GeometryTypes.POINTZM:
-        case Geometry.GeometryTypes.POINT25D:
-          geometryType = 'Point';
-          break;
-        case Geometry.GeometryTypes.MULTIPOINTZ:
-        case Geometry.GeometryTypes.MULTIPOINTM:
-        case Geometry.GeometryTypes.MULTIPOINTZM:
-        case Geometry.GeometryTypes.MULTIPOINT25D:
-          geometryType = 'MultiPoint';
-          break;
-        case Geometry.GeometryTypes.POLYGONZ:
-        case Geometry.GeometryTypes.POLYGONM:
-        case Geometry.GeometryTypes.POLYGONZM:
-        case Geometry.GeometryTypes.POLYGON25D:
-          geometryType = 'Polygon';
-          break;
-        case Geometry.GeometryTypes.MULTIPOLYGONZ:
-        case Geometry.GeometryTypes.MULTIPOLYGONM:
-        case Geometry.GeometryTypes.MULTIPOLYGONZM:
-        case Geometry.GeometryTypes.MULTIPOLYGON25D:
-          geometryType = 'MultiPolygon';
-          break;
-      }
+      const geometryType = Geometry.getOLGeometry(originalGeometryType);
       const source = editingLayer.getSource();
       const attributes = originalLayer.getEditingFields();
       const temporarySource = new ol.source.Vector();
@@ -103,74 +54,11 @@ proto.run = function(inputs, context) {
           source.addFeature(feature);
           session.pushAdd(layerId, feature);
         } else feature = e.feature;
-        const originalFeatureCoordinates = feature.getGeometry().getCoordinates();
-        switch (originalGeometryType){
-          //POINT //[x,y]
-          case Geometry.GeometryTypes.POINTZ:
-          case Geometry.GeometryTypes.POINTM:
-          case Geometry.GeometryTypes.POINTZM:
-          case Geometry.GeometryTypes.POINT25D:
-            originalFeatureCoordinates.push(0);
-            feature.getGeometry().setCoordinates(originalFeatureCoordinates);
-            break;
-          //MULTIPOINT [[x1, y1], [x2, y2]]
-          case Geometry.GeometryTypes.MULTIPOINTZ:
-          case Geometry.GeometryTypes.MULTIPOINTM:
-          case Geometry.GeometryTypes.MULTIPOINTZM:
-          case Geometry.GeometryTypes.MULTIPOINT25D:
-          //LINE [[x1, y1], [x2, y2]]
-          case Geometry.GeometryTypes.LINESTRINGZ:
-          case Geometry.GeometryTypes.LINESTRINGM:
-          case Geometry.GeometryTypes.LINESTRINGZM:
-          case Geometry.GeometryTypes.LINESTRING25D:
-          case Geometry.GeometryTypes.LINEZ:
-          case Geometry.GeometryTypes.LINEM:
-          case Geometry.GeometryTypes.LINEZM:
-          case Geometry.GeometryTypes.LINE25D:
-            originalFeatureCoordinates.forEach(coordinates => coordinates.push(0));
-            feature.getGeometry().setCoordinates(originalFeatureCoordinates);
-            break;
-          //MULTILINE [
-          // [[x1, y1],[x2, y2]],
-          // [[x3, y3], [x4, y4]]
-          // ]
-          case Geometry.GeometryTypes.MULTILINESTRINGZ:
-          case Geometry.GeometryTypes.MULTILINESTRINGM:
-          case Geometry.GeometryTypes.MULTILINESTRINGZM:
-          case Geometry.GeometryTypes.MULTILINESTRING25D:
-          case Geometry.GeometryTypes.MULTILINEZ:
-          case Geometry.GeometryTypes.MULTILINEM:
-          case Geometry.GeometryTypes.MULTILINEZM:
-          case Geometry.GeometryTypes.MULTILINE25D:
-            originalFeatureCoordinates.forEach(singleLine => {
-              singleLine.forEach(coordinates => coordinates.push(0))
-            });
-            feature.getGeometry().setCoordinates(originalFeatureCoordinates);
-            break;
-          //POLYGON [
-          //[[x1, y1], [x2, y2], [x3, y3], [x1, y1]]
-          // ]
-          case Geometry.GeometryTypes.POLYGONZ:
-          case Geometry.GeometryTypes.POLYGONM:
-          case Geometry.GeometryTypes.POLYGONZM:
-          case Geometry.GeometryTypes.POLYGON25D:
-            originalFeatureCoordinates[0].forEach(coordinates => coordinates.push(0));
-            feature.getGeometry().setCoordinates(originalFeatureCoordinates);
-            break;
-          //MULTIPOLYGON  [
-          //       [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ],
-          //       [ [100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2] ]
-          //       ]
-          case Geometry.GeometryTypes.MULTIPOLYGONZ:
-          case Geometry.GeometryTypes.MULTIPOLYGONM:
-          case Geometry.GeometryTypes.MULTIPOLYGOZM:
-          case Geometry.GeometryTypes.MULTIPOLYGON25D:
-            originalFeatureCoordinates.forEach(singlePolygon => {
-              singlePolygon[0].forEach(coordinates => coordinates.push(0))
-            });
-            feature.getGeometry().setCoordinates(originalFeatureCoordinates);
-            break;
-        }
+        // set Z values based on layer Geoemtry
+        feature = Geometry.addZValueToOLFeatureGeometry({
+          feature,
+          geometryType: originalGeometryType
+        });
         inputs.features.push(feature);
         this.fireEvent('addfeature', feature); // emit event to get from subscribers
         d.resolve(inputs);
