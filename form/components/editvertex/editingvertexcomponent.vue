7fe23935423a1d5a44201405022f8bb9365d98f0
volterra79 <boccacci.francesco@gmail.com>
2021-12-22 16:02:52 +0100

    add handle of 3 type of coordinate format

--- form/components/editvertex/editingvertexcomponent.vue
+++ form/components/editvertex/editingvertexcomponent.vue
@@ -9,9 +9,21 @@
                     <button class="btn skin-button" style="width: 50px;" @mouseover="highLightVertex(index)" @click="zoomToVertex(index)">
                         <i :class="g3wtemplate.font['marker']"></i>
                     </button>
-                    <div style="width:100%; display: flex; justify-content: space-between; margin-left: 5px;">
-                        <input style="margin-right: 5px;" class="form-control" type="number" @change="changeVertexCoordinates(index, v)" v-model="v.coordinates[0]"/>
-                        <input class="form-control" type="number" @change="changeVertexCoordinates(index, v)" v-model="v.coordinates[1]">
+                    <div style="width:100%; display: flex; justify-content: space-around; margin-left: 5px;">
+                        <div>
+                            <h5 style="font-weight: bold">DEGREE</h5>
+                            <input style="margin-right: 5px;" class="form-control" type="number" @change="changeVertexCoordinates(index, v)" v-model="v.coordinates[0]"/>
+                            <input class="form-control" type="number" @change="changeVertexCoordinates(index, v)" v-model="v.coordinates[1]">
+                        </div>
+                        <div>
+                            <h5 style="font-weight: bold">DMS</h5>
+                            <input style="margin-right: 5px;" class="form-control" readonly="true" :value="v.coordinatesDHMS"/>
+                        </div>
+                        <div>
+                            <h5 style="font-weight: bold">EPSG:3857</h5>
+                            <input style="margin-right: 5px;" class="form-control"  readonly="true" v-model="v.coordinates3857[0]"/>
+                            <input style="margin-right: 5px;" class="form-control"  readonly="true" v-model="v.coordinates3857[1]"/>
+                        </div>
                     </div>
                 </div>
                 <div style="width: 100%" class="fields_content">
@@ -39,10 +51,18 @@
             }
         },
         methods: {
+            toDHMS(vertex){
+                vertex.coordinatesDHMS = ol.coordinate.toStringHDMS(vertex.coordinates);
+            },
+            to3857(vertex){
+                vertex.coordinates3857 = ol.proj.transform(vertex.coordinates, 'EPSG:4326', 'EPSG:3857');
+            },
             changeVertexCoordinates(index, vertex){
               vertex.coordinates[0] = 1* vertex.coordinates[0];
               vertex.coordinates[1] = 1* vertex.coordinates[1];
-              this.featureVertex[index].getGeometry().setCoordinates(vertex.coordinates)
+              this.featureVertex[index].getGeometry().setCoordinates(vertex.coordinates);
+              this.toDHMS(vertex);
+              this.to3857(vertex);
             },
             close(){
                 GUI.popContent();
@@ -78,19 +98,23 @@
             this.originalVertexCoordinates = [];
             this.featureVertex = vertexLayerToolBox.getEditingLayerSource().getFeatures().filter(feature => feature.get('feature_id') == id);
             this.featureVertex.forEach(feature =>{
-                const fields = [];
-                const coordinates = feature.getGeometry().getCoordinates();
-                this.originalVertexCoordinates.push([...coordinates]);
+                const vertex = {
+                    fields: [],
+                    coordinates: null,
+                    coordinatesDHMS: null,
+                    coordinates3857: null
+                };
+                vertex.coordinates = feature.getGeometry().getCoordinates();
+                this.toDHMS(vertex);
+                this.to3857(vertex);
+                this.originalVertexCoordinates.push([...vertex.coordinates]);
                 vertexLayer.getFieldsWithValues(feature).forEach(({name, editable,value}) =>{
-                    editable && fields.push({
+                    editable && vertex.fields.push({
                         name,
                         value
                     })
                 });
-                this.vertex.push({
-                    fields,
-                    coordinates
-                })
+                this.vertex.push(vertex);
             })
         },
         async mounted() {
