d1285845551b7f076eaa5f213bb5e91dfc9a4029
volterra79 <boccacci.francesco@gmail.com>
2022-06-17 12:48:01 +0200

    fix issue on relation vector layer on add relation and move child relation

--- workflows/steps/tasks/movefeaturettask.js
+++ workflows/steps/tasks/movefeaturettask.js
@@ -1,5 +1,4 @@
-const inherit = g3wsdk.core.utils.inherit;
-const base =  g3wsdk.core.utils.base;
+const {base, inherit} = g3wsdk.core.utils;
 const EditingTask = require('./editingtask');
 
 function MoveFeatureTask(options){
