146f20e558c2f0db5b48f6d994176ba9ee4e6564
volterra79 <boccacci.francesco@gmail.com>
2023-09-12 08:42:57 +0200

    Manage delete parent features and unlink relation features

--- workflows/steps/tasks/deletefeaturetask.js
+++ workflows/steps/tasks/deletefeaturetask.js
@@ -20,47 +20,76 @@ proto.run = function(inputs, context) {
   const feature = inputs.features[0];
   const EditingService = require('../../../services/editingservice');
   const RelationService = require('../../../services/relationservice');
+
   const relations = EditingService._filterRelationsInEditing({
     layerId,
-    relations: originaLayer.getRelations() ? originaLayer.getRelations().getArray() : []
+    relations: originaLayer.getRelations() ?
+      originaLayer.getRelations().getArray() :
+      []
   }).filter(relation => {
+    //get Relation Id
     const relationId = EditingService._getRelationId({
       layerId,
       relation
     });
+
     const relationLayer = EditingService.getLayerById(relationId);
+
     const {ownField} = EditingService._getRelationFieldsFromRelation({
       layerId: relationId,
       relation
     });
-    const field = relationLayer.getEditingFields().find((field) => {
-      return field.name === ownField;
-    });
-    return !field.validate.required;
+
+    //Exclude relation child layer that has at least one
+    //editing field required because when unlink relation feature from
+    // delete father, when try to commit update relation, we receive an error
+    //due missing value /null to required field.
+
+    return ownField.reduce((bool, oField) => {
+      const field = relationLayer.getEditingFields()
+        .find((field) => field.name === oField);
+      return bool || !field.validate.required;
+    }, false)
+
   });
-  const promise = relations.length ? EditingService.getLayersDependencyFeatures(layerId, {
-    feature,
-    relations
-  }) : Promise.resolve();
+
+  const promise = relations.length > 0 ?
+    EditingService.getLayersDependencyFeatures(layerId, {feature, relations}) :
+    Promise.resolve();
+
+  //promise return features relations and add to relation layer child
   promise.then(() => {
+
+    //get data features
     const relationsInEditing = EditingService.getRelationsInEditing({
       layerId,
       relations,
       feature,
     });
+
     inputs.features = [feature];
-    relationsInEditing.forEach((relationInEditing) => {
+
+    relationsInEditing.forEach(relationInEditing => {
+      //relation is an instance of Relation.
+      //relations are relations features
       const {relation, relations} = relationInEditing;
+
       const relationService = new RelationService(layerId, {
         relation,
         relations
       });
+
       const relationsLength = relations.length;
+
+      //Unlink relation features related to layer id
       for (let index = 0; index < relationsLength ; index++) {
+        //unlink
         relationService.unlinkRelation(0, false)
       }
     });
+
     session.pushDelete(layerId, feature);
+
     d.resolve(inputs);
   });
 
