0797846421ab031ea170806c890cde9e1cbe827b
Raruto <Raruto@users.noreply.github.com>
2023-09-22 08:09:51 +0200

    code format

--- form/editingform.js
+++ form/editingform.js
@@ -1,42 +1,51 @@
-const {base, inherit} = g3wsdk.core.utils;
-const {GUI} = g3wsdk.gui;
-const {FormComponent} = g3wsdk.gui.vue;
+const { base, inherit }  = g3wsdk.core.utils;
+const { GUI }            = g3wsdk.gui;
+const { FormComponent }  = g3wsdk.gui.vue;
 const EditingFormService = require('./editingformservice');
 
-function EditingFormComponent(options={}) {
-  base(this, options);
-  const EditingService = require('../services/editingservice');
-  const relationsOptions = options.context_inputs || null;
-  const {layer} = options;
-  const layerId = layer.getId();
-  if (relationsOptions) {
-    const feature = relationsOptions.inputs.features[relationsOptions.inputs.features.length-1];
-    const promise =  feature.isNew() ?
-      Promise.resolve() :
-      EditingService.getLayersDependencyFeatures(layerId, {feature, filterType: 'fid'});
+function EditingFormComponent(options = {}) {
 
-    promise.then(() => {
+  base(this, options);
+
+  const EditingService   = require('../services/editingservice');
+  const relationsOptions = options.context_inputs || null;
+  const layerId          = options.layer.getId();
+
+  if (relationsOptions) {
+    (
+      feature.isNew()
+        ? Promise.resolve()
+        : EditingService.getLayersDependencyFeatures(layerId, {
+          feature: relationsOptions.inputs.features[relationsOptions.inputs.features.length-1],
+          filterType: 'fid'
+        })
+    ).then(() => {
+
       relationsOptions.formEventBus = this.getService().getEventBus();
-      const service = new EditingFormService(relationsOptions);
-      const RelationComponents = service.buildRelationComponents();
-      const customFormComponents = EditingService.getFormComponentsById(layerId);
-      //check if add components to add
+
+      const service                 = new EditingFormService(relationsOptions);
+      const RelationComponents      = service.buildRelationComponents();
+      const customFormComponents    = EditingService.getFormComponentsById(layerId);
+
+      // check if add components to add
       if (customFormComponents.length > 0) {
         this.addFormComponents(customFormComponents);
       }
+
       // add relation component
       if (RelationComponents.length > 0)  {
         this.addFormComponents(RelationComponents);
       }
-      //Method that overwrite handle relation on click
-      this.getService().handleRelation = async function({relation, layerId, feature}) {
+
+      // overwrite click on relation handler   
+      this.getService().handleRelation = async function({ relation, layerId, feature }) {
         GUI.setLoadingContent(true);
-        const {name: relationId} = relation;
-        await EditingService.setLayerUniqueFieldValues(layer.getRelationById(relationId).getChild());
-        this.setCurrentComponentById(relationId);
+        await EditingService.setLayerUniqueFieldValues(options.layer.getRelationById(relation.name).getChild());
+        this.setCurrentComponentById(relation.name);
         GUI.setLoadingContent(false);
       };
-    })
+
+    });
   }
 }
 
--- form/editingformservice.js
+++ form/editingformservice.js
@@ -1,29 +1,44 @@
-const {GUI} = g3wsdk.gui;
-const RelationComponent = require('../components/FormRelation.vue');
-const EditingFormService = function(options={}) {
+const { GUI }            = g3wsdk.gui;
+const RelationComponent  = require('../components/FormRelation.vue');
+
+const EditingFormService = function(options = {}) {
+
   const EditingService = require('../services/editingservice');
+
   this.state = {
     relations: []
   };
-  const {layer, features} = options.inputs || {};
-  // get back to Father function
+
+  const { layer, features } = options.inputs || {};
+
+  /**
+   * get back to Father function
+   */
   this._formEventBus = options.formEventBus || null;
+
   const layerId = layer.getId();
-  // get feature
-  const feature = features[features.length - 1];
+
   // get only relation with type not ONE and layer is the father
   let relations = layer.getRelations().getArray().filter(relation => relation.getType() !== 'ONE' && relation.getFather() === layerId);
-  /**
-   * get relation layers that set in editing on g3w-admin
-   */
-  relations = EditingService.getRelationsInEditing({layerId, relations , feature});
+
+  // get relation layers that set in editing on g3w-admin
+  relations = EditingService.getRelationsInEditing({
+    layerId,
+    relations ,
+    feature: features[features.length - 1],
+  });
+
   this.hasRelations = () => relations.length > 0;
+
   this.buildRelationComponents = function() {
+
     const self = this;
+
     const relationComponents = [];
-    relations.forEach(relation => {
+
+    relations.forEach(({ relation, relations }) => {
       const relationComponent = Vue.extend({
-        mixins: [RelationComponent],
+        mixins: [ RelationComponent ],
         name: `relation_${Date.now()}`,
         methods: {
           getService() {
@@ -33,23 +48,26 @@ const EditingFormService = function(options={}) {
         data() {
           return {
             layerId,
-            relation: relation.relation,
-            relations: relation.relations,
+            relation,
+            relations,
             resourcesurl: GUI.getResourcesUrl(),
             formeventbus: self._formEventBus
-          }
+          };
         },
       });
       relationComponents.push({
-        title: "plugins.editing.edit_relation",
-        name: relation.relation.name,
-        id: relation.relation.id,
-        header: false, // not show to header form
+        title:     "plugins.editing.edit_relation",
+        name:      relation.name,
+        id:        relation.id,
+        header:    false,            // hide header form
         component: relationComponent
       })
     });
+
     return relationComponents;
+
   };
+
 };
 
 module.exports =  EditingFormService;
