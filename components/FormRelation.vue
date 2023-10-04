e697f0d600a2836d376dab53a72b51d1d7f68fbb
volterra79 <boccacci.francesco@gmail.com>
2023-10-04 14:36:01 +0200

    Show bar loader component when get key values data from server

--- components/FormRelation.vue
+++ components/FormRelation.vue
@@ -7,225 +7,231 @@
       v-if  = "active"
       style = "margin-bottom: 5px;"
     >
-
-      <!-- RELATION TITLE -->
-      <div
-        ref   = "relation_header_title"
-        class = "box-header with-border skin-color"
-        style = "
-          width: 100%;
-          display: flex;
-          font-weight: bold;
-          font-size: 1.3em;
-          align-items: center;
-          background-color: #fff;
-        "
-      >
-        <span v-t-plugin="'editing.edit_relation'"></span>
-        <span style="margin-left: 2px;">: {{relation.name.toUpperCase()}}</span>
-      </div>
-
-      <!-- RELATION TOOLS -->
-      <div
-        ref   = "relation_header_tools"
-        class = "box-header with-border"
-        style = "
-          width: 100%;
-          display: flex;
-          background-color: #fff;
-        "
-      >
-        <!-- SEARCH BOX -->
-        <div id="search-box" style="margin-right: auto;">
-          <input
-            v-if         = "relationsLength"
-            type         = "text"
-            class        = "form-control"
-            id           = "filterRelation"
-            :placeholder = "placeholdersearch"
-          >
-        </div>
-
-        <div style="display: flex; justify-content: flex-end">
-
-          <!-- CHANGE ATTRIBUTE -->
-          <span
-            v-if                      = "undefined !== capabilities.relation.find(capability => 'change_attr_feature' === capability)"
-            class                     = "g3w-icon add-link"
-            align                     = "center"
-            v-t-tooltip:bottom.create = "tooltips.link_relation"
-            @click.stop               = "enableAddLinkButtons ? linkRelation() : null"
-            :class                    = "[
-              { 'disabled': !enableAddLinkButtons },
-              g3wtemplate.font['link']
-            ]"
-          ></span>
-
-          <!-- ADD FEATURE -->
-          <span
-            v-if                      = "undefined !== capabilities.relation.find(capability => 'add_feature' === capability)"
-            v-t-tooltip:bottom.create = "tooltips.add_relation"
-            @click                    = "enableAddLinkButtons ? addRelationAndLink() : null"
-            class                     = "g3w-icon add-link pull-right"
-            :class                    = "[
-              { 'disabled' : !enableAddLinkButtons },
-              g3wtemplate.font['plus']
-            ]"
-          ></span>
-
-        </div>
-
-      </div>
-
-      <!-- VECTOR RELATION TOOLS -->
-      <section
-        v-if  = "showAddVectorRelationTools"
-        ref   = "relation_vector_tools"
-        style = "
-          display: flex;
-          flex-direction: column;
-          border: 2px solid #eee;
-          background-color: #fff;
-          padding: 10px;
-        "
-      >
-
-        <!-- ADD VECTOR RELATION -->
-        <div>
-          <div class="g3w-editing-new-relation-vector-type" v-t-plugin="'editing.relation.draw_new_feature'"></div>
-          <button
-            class       = "btn skin-button"
-            style       = "width: 100%"
-            @click.stop = "addVectorRelation"
-          >
-            <i :class="g3wtemplate.font['pencil']"></i>
-          </button>
-        </div>
-
-        <divider/>
-
-        <div style="align-self: center" v-t-plugin="'editing.relation.draw_or_copy'"></div>
-
-        <divider/>
-
+      <template v-if="loading">
+        <bar-loader :loading="loading"/>
+      </template>
+      <template v-else>
+        <!-- RELATION TITLE -->
         <div
-          id    = "g3w-select-editable-layers-content"
-          style = "
-            flex-grow: 1;
-            display: flex;
-            flex-direction: column
-          "
+                ref   = "relation_header_title"
+                class = "box-header with-border skin-color"
+                style = "
+        width: 100%;
+        display: flex;
+        font-weight: bold;
+        font-size: 1.3em;
+        align-items: center;
+        background-color: #fff;
+      "
         >
-
-          <div class="g3w-editing-new-relation-vector-type" v-t-plugin="'editing.relation.copy_feature_from_other_layer'"></div>
-
-          <select
-            id        = "g3w-select-editable-layers-to-copy"
-            v-select2 = "'copylayerid'"
-          >
-            <option
-              v-for  = "copyFeatureLayer in copyFeatureLayers"
-              :key   = "copyFeatureLayer.id"
-              :value = "copyFeatureLayer.id"
-            >{{copyFeatureLayer.name}}</option>
-          </select>
-
-          <!-- COPY FEATURE FROM OTHER LAYER -->
-          <button
-            class       = "btn skin-button"
-            @click.stop = "copyFeatureFromOtherLayer"
-          >
-            <i :class="g3wtemplate.font['clipboard']"></i>
-          </button>
+            <span v-t-plugin="'editing.edit_relation'"></span>
+            <span style="margin-left: 2px;">: {{relation.name.toUpperCase()}}</span>
         </div>
 
-      </section>
-
-      <!-- RELATION CONTENT -->
-      <div
-        ref   = "relation_body"
-        class = "box-body"
-        style = "padding:0;"
-      >
-        <table
-          v-if  = "showTable"
-          class = "table g3wform-relation-table table-striped"
-          style = "width:100%"
+        <!-- RELATION TOOLS -->
+        <div
+                ref   = "relation_header_tools"
+                class = "box-header with-border"
+                style = "
+        width: 100%;
+        display: flex;
+        background-color: #fff;
+      "
         >
-          <thead>
-            <tr>
-              <th v-t="'tools'"></th>
-              <th></th>
-              <th v-for="attribute in relationAttributesSubset(relations[0])">{{attribute.label}}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for = "(relation, index) in relations"
-              class = "featurebox-header"
-            >
-              <td>
-                <div style="display: flex">
-                  <div
-                    v-for              = "relationtool in getRelationTools()"
-                    :key               = "relationtool.state.name"
-                    class              = "skin-tooltip-right editbtn enabled"
-                    @click.stop        = "startTool(relationtool, index)"
-                    data-toggle        = "tooltip"
-                    data-placement     = "right"
-                    v-t-tooltip:plugin = "relationtool.state.name"
-                  >
-                    <img
-                      height = "20px"
-                      width  = "20px"
-                      :src   = "`${resourcesurl}images/${relationtool.state.icon}`"
-                    />
-                  </div>
-                </div>
-              </td>
-              <td class="action-cell">
-                <div
-                  v-if                     = "!fieldrequired && undefined !== capabilities.relation.find(capability => 'change_attr_feature' === capability)"
-                  class                    = "g3w-mini-relation-icon g3w-icon"
-                  :class                   = "g3wtemplate.font['unlink']"
-                  @click.stop              = "unlinkRelation(index)"
-                  v-t-tooltip:right.create = "tooltips.unlink_relation"
-                  aria-hidden              = "true"
-                ></div>
-              </td>
-              <td
-                v-for  = "attribute in relationAttributesSubset(relation)"
-                v-show = "!showAllFields(index)"
-              >
-                <!-- MEDIA ATTRIBUTE-->
-                <div
-                  v-if = "isMedia(attribute.value) && getValue(attribute.value)"
-                  class = "preview"
+            <!-- SEARCH BOX -->
+            <div id="search-box" style="margin-right: auto;">
+                <input
+                        v-if         = "relationsLength"
+                        type         = "text"
+                        class        = "form-control"
+                        id           = "filterRelation"
+                        :placeholder = "placeholdersearch"
                 >
-                  <a :href="getValue(attribute.value)" target="_blank">
-                    <div class="previewtype" :class="getMediaType(attribute.value.mime_type).type">
-                      <i class="fa-2x" :class="g3wtemplate.font[getMediaType(attribute.value.mime_type).type]"></i>
-                    </div>
-                  </a>
-                  <div class="filename">{{ getFileName(attribute.value) }}</div>
-                </div>
-                <!-- LINK ATTRIBUTE -->
-                <a
-                  v-else-if = "isLink(attribute)"
-                  :href     = "getValue(attribute.value)"
-                  target    = "_blank">{{ getValue(attribute.value) }}
-                </a>
-                <!-- TEXTUAL ATTRIBUTE -->
-                <span v-else>{{ getValueFromKeyValueWidget(relation, attribute.name) }}</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+            </div>
+
+            <div style="display: flex; justify-content: flex-end">
+
+                <!-- CHANGE ATTRIBUTE -->
+                <span
+                        v-if                      = "undefined !== capabilities.relation.find(capability => 'change_attr_feature' === capability)"
+                        class                     = "g3w-icon add-link"
+                        align                     = "center"
+                        v-t-tooltip:bottom.create = "tooltips.link_relation"
+                        @click.stop               = "enableAddLinkButtons ? linkRelation() : null"
+                        :class                    = "[
+            { 'disabled': !enableAddLinkButtons },
+            g3wtemplate.font['link']
+          ]"
+                ></span>
+
+                <!-- ADD FEATURE -->
+                <span
+                        v-if                      = "undefined !== capabilities.relation.find(capability => 'add_feature' === capability)"
+                        v-t-tooltip:bottom.create = "tooltips.add_relation"
+                        @click                    = "enableAddLinkButtons ? addRelationAndLink() : null"
+                        class                     = "g3w-icon add-link pull-right"
+                        :class                    = "[
+            { 'disabled' : !enableAddLinkButtons },
+            g3wtemplate.font['plus']
+          ]"
+                ></span>
+
+            </div>
+
+        </div>
+
+        <!-- VECTOR RELATION TOOLS -->
+        <section
+                v-if  = "showAddVectorRelationTools"
+                ref   = "relation_vector_tools"
+                style = "
+        display: flex;
+        flex-direction: column;
+        border: 2px solid #eee;
+        background-color: #fff;
+        padding: 10px;
+      "
+        >
+
+            <!-- ADD VECTOR RELATION -->
+            <div>
+                <div class="g3w-editing-new-relation-vector-type" v-t-plugin="'editing.relation.draw_new_feature'"></div>
+                <button
+                        class       = "btn skin-button"
+                        style       = "width: 100%"
+                        @click.stop = "addVectorRelation"
+                >
+                    <i :class="g3wtemplate.font['pencil']"></i>
+                </button>
+            </div>
+
+            <divider/>
+
+            <div style="align-self: center" v-t-plugin="'editing.relation.draw_or_copy'"></div>
+
+            <divider/>
+
+            <div
+                    id    = "g3w-select-editable-layers-content"
+                    style = "
+          flex-grow: 1;
+          display: flex;
+          flex-direction: column
+        "
+            >
+
+                <div class="g3w-editing-new-relation-vector-type" v-t-plugin="'editing.relation.copy_feature_from_other_layer'"></div>
+
+                <select
+                        id        = "g3w-select-editable-layers-to-copy"
+                        v-select2 = "'copylayerid'"
+                >
+                    <option
+                            v-for  = "copyFeatureLayer in copyFeatureLayers"
+                            :key   = "copyFeatureLayer.id"
+                            :value = "copyFeatureLayer.id"
+                    >{{copyFeatureLayer.name}}</option>
+                </select>
+
+                <!-- COPY FEATURE FROM OTHER LAYER -->
+                <button
+                        class       = "btn skin-button"
+                        @click.stop = "copyFeatureFromOtherLayer"
+                >
+                    <i :class="g3wtemplate.font['clipboard']"></i>
+                </button>
+            </div>
+
+        </section>
+
+        <!-- RELATION CONTENT -->
+        <div
+                ref   = "relation_body"
+                class = "box-body"
+                style = "padding:0;"
+        >
+            <table
+                    v-if  = "showTable"
+                    class = "table g3wform-relation-table table-striped"
+                    style = "width:100%"
+            >
+                <thead>
+                <tr>
+                    <th v-t="'tools'"></th>
+                    <th></th>
+                    <th v-for="attribute in relationAttributesSubset(relations[0])">{{attribute.label}}</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr
+                        v-for = "(relation, index) in relations"
+                        class = "featurebox-header"
+                >
+                    <td>
+                        <div style="display: flex">
+                            <div
+                                    v-for              = "relationtool in getRelationTools()"
+                                    :key               = "relationtool.state.name"
+                                    class              = "skin-tooltip-right editbtn enabled"
+                                    @click.stop        = "startTool(relationtool, index)"
+                                    data-toggle        = "tooltip"
+                                    data-placement     = "right"
+                                    v-t-tooltip:plugin = "relationtool.state.name"
+                            >
+                                <img
+                                        height = "20px"
+                                        width  = "20px"
+                                        :src   = "`${resourcesurl}images/${relationtool.state.icon}`"
+                                />
+                            </div>
+                        </div>
+                    </td>
+                    <td class="action-cell">
+                        <div
+                                v-if                     = "!fieldrequired && undefined !== capabilities.relation.find(capability => 'change_attr_feature' === capability)"
+                                class                    = "g3w-mini-relation-icon g3w-icon"
+                                :class                   = "g3wtemplate.font['unlink']"
+                                @click.stop              = "unlinkRelation(index)"
+                                v-t-tooltip:right.create = "tooltips.unlink_relation"
+                                aria-hidden              = "true"
+                        ></div>
+                    </td>
+                    <td
+                            v-for  = "attribute in relationAttributesSubset(relation)"
+                            v-show = "!showAllFields(index)"
+                    >
+                        <!-- MEDIA ATTRIBUTE-->
+                        <div
+                                v-if = "isMedia(attribute.value) && getValue(attribute.value)"
+                                class = "preview"
+                        >
+                            <a :href="getValue(attribute.value)" target="_blank">
+                                <div class="previewtype" :class="getMediaType(attribute.value.mime_type).type">
+                                    <i class="fa-2x" :class="g3wtemplate.font[getMediaType(attribute.value.mime_type).type]"></i>
+                                </div>
+                            </a>
+                            <div class="filename">{{ getFileName(attribute.value) }}</div>
+                        </div>
+                        <!-- LINK ATTRIBUTE -->
+                        <a
+                                v-else-if = "isLink(attribute)"
+                                :href     = "getValue(attribute.value)"
+                                target    = "_blank">{{ getValue(attribute.value) }}
+                        </a>
+                        <!-- TEXTUAL ATTRIBUTE -->
+                        <span v-else>{{ getValueFromKeyValueWidget(relation, attribute.name) }}</span>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+      </template>
     </div>
 </template>
 
 <script>
+  import EditingService from "editing/services/editingservice";
+
   const { tPlugin: t } = g3wsdk.core.i18n;
   const { toRawType }   = g3wsdk.core.utils;
   const { Layer }       = g3wsdk.core.layer;
@@ -250,6 +256,7 @@
 
     data() {
       return {
+        loading : false,
         showAddVectorRelationTools: false,
         copylayerid:                null, //used for vector relation layer
         active:                     false,
@@ -564,16 +571,26 @@
 
       if (!this.loadEventuallyRelationValuesForInputs) {
         const EditingService = require('../services/editingservice');
-        await EditingService.runEventHandler({
-          type: 'show-relation-editing',
-          id:   EditingService._getRelationId({
-            layerId: this.layerId,
-            relation: this.relation
-          }),
-          component: this,
-        });
+        this.loading = true;
+        try {
+          await EditingService.runEventHandler({
+            type: 'show-relation-editing',
+            id:   EditingService._getRelationId({
+              layerId: this.layerId,
+              relation: this.relation
+            }),
+            component: this,
+          });
+        } catch(err) {
+          console.warn(err)
+        }
+
+        this.loading = false;
+
         this.loadEventuallyRelationValuesForInputs = true;
       }
+
+
 
       await this.$nextTick();
 
