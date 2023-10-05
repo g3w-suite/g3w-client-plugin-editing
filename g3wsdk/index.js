77d43ad171f5833a208f76ff855bf34690cd2386
Raruto <Raruto@users.noreply.github.com>
2023-10-05 14:29:22 +0200

    console.log

--- g3wsdk/index.js
+++ g3wsdk/index.js
@@ -7,7 +7,7 @@ import Session          from './session';
  * Editing API will be removed from core after g3w-client@v.4.x 
  */
 if (window.g3wsdk.core.editing || g3wsdk.version < '4') {
-  console.log('g3wsdk.core.editing is being overwritten');
+  console.log('g3wsdk.core.editing will be overwritten');
 }
 
 window.g3wsdk.core.editing = {
