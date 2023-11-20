f5c55340ada35b98250d34a8b5e85e9feb14a26f
volterra79 <boccacci.francesco@gmail.com>
2023-11-20 10:24:21 +0100

    Clean code

--- components/CopyFeaturesFromOtherLayers.vue
+++ components/CopyFeaturesFromOtherLayers.vue
@@ -17,74 +17,74 @@
     </div>
     <section class="g3w-editing-other-layers-features" :style="{height: `${$data._height}px`}">
       <div
-                v-for = "(layerId) in Object.keys($options.layers)"
+        v-for = "(layerId) in Object.keys($options.layers)"
         >
 
-            <div class="skin-color" style="font-weight: bold; font-size: 1.2em;">{{ getLayerTitle(layerId) }}</div>
+          <div class="skin-color" style="font-weight: bold; font-size: 1.2em;">{{ getLayerTitle(layerId) }}</div>
 
-            <divider />
+          <divider />
 
-            <div
-                    class = "copy-features-for-layer-content"
-                    style = "overflow-x: auto"
-            >
-                <div
-                        v-for = "(feature, index) in $options.layers[layerId].features"
-                        style = "
-            padding: 5px;
-            position: relative;
+          <div
+            class = "copy-features-for-layer-content"
+            style = "overflow-x: auto"
+          >
+              <div
+                v-for = "(feature, index) in $options.layers[layerId].features"
+                style = "
+    padding: 5px;
+    position: relative;
+    display: flex;
+    align-items: baseline;
+    border-bottom: 1px solid #eee;
+  "
+              >
+
+                  <div
+                    style = "
             display: flex;
-            align-items: baseline;
-            border-bottom: 1px solid #eee;
+            flex-direction: column;
+            border-right: 1px solid #eee;
           "
-                >
+                  >
+                      <div
+                        @click.stop = "zoomToFeature(feature)"
+                        :class      = "g3wtemplate.font['marker']"
+                        class       = "skin-color"
+                        style       = "
+        padding: 0 5px 15px 5px;
+        font-size: 1.1em;
+        cursor: pointer;
+        margin-right: 5px;
+      "
+                      ></div>
+                      <div v-t-tooltip:right.create="'plugins.editing.steps.help.select_elements'">
+                          <input
+                            @click.stop = "selectFeature(feature)"
+                            :id         = "`${layerId}_${index}_select_feature_from_layer`"
+                            type        = "checkbox"
+                            class       = "magic-checkbox"
+                          >
+                          <label
+                            :for  = "`${layerId}_${index}_select_feature_from_layer`"
+                            style = "color: #FFF"> {{ feature.getId() || 0 }}
+                          </label>
+                      </div>
+                  </div>
 
-                    <div
-                            style = "
-              display: flex;
-              flex-direction: column;
-              border-right: 1px solid #eee;
-            "
-                    >
-                        <div
-                                @click.stop = "zoomToFeature(feature)"
-                                :class      = "g3wtemplate.font['marker']"
-                                class       = "skin-color"
-                                style       = "
-                padding: 0 5px 15px 5px;
-                font-size: 1.1em;
-                cursor: pointer;
-                margin-right: 5px;
-              "
-                        ></div>
-                        <div v-t-tooltip:right.create="'plugins.editing.steps.help.select_elements'">
-                            <input
-                                    @click.stop = "selectFeature(feature)"
-                                    :id         = "`${layerId}_${index}_select_feature_from_layer`"
-                                    type        = "checkbox"
-                                    class       = "magic-checkbox"
-                            >
-                            <label
-                                    :for  = "`${layerId}_${index}_select_feature_from_layer`"
-                                    style = "color: #FFF"> {{ feature.getId() || 0 }}
-                            </label>
-                        </div>
-                    </div>
+                  <div
+                    v-for = "({ attribute, value }) in getAttributesFeature(feature, layerId)"
+                    style = "
+      display: flex;
+      flex-direction: column;
+      padding: 10px;
+    "
+                  >
+                    <span style="font-weight: bold; margin-bottom: 10px;">{{ attribute }}</span>
+                    <span style="align-self: start">{{ value }}</span>
+                  </div>
 
-                    <div
-                            v-for = "({ attribute, value }) in getAttributesFeature(feature, layerId)"
-                            style = "
-              display: flex;
-              flex-direction: column;
-              padding: 10px;
-            "
-                    >
-                        <span style="font-weight: bold; margin-bottom: 10px;">{{ attribute }}</span>
-                        <span style="align-self: start">{{ value }}</span>
-                    </div>
-
-                </div>
-            </div>
+              </div>
+          </div>
 
         </div>
     </section>
@@ -115,7 +115,7 @@
 
     methods: {
       resize() {
-        this.$nextTick(()=> this.$data._height = window.innerHeight - 200)
+        this.$nextTick(() => this.$data._height = window.innerHeight - 200);
       },
       getAttributesFeature(feature, layerId) {
         const { external, fields } = this.$options.layers[layerId];
