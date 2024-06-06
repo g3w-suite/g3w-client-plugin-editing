b8589ff40fdd433a72595468ef9807cadf26e055
volterra79 <boccacci.francesco@gmail.com>
2024-06-06 11:59:54 +0200

    Check if is a 3D Geometry

--- utils/convertFeaturesGeometryToGeometryTypeOfLayer.js
+++ utils/convertFeaturesGeometryToGeometryTypeOfLayer.js
@@ -11,11 +11,20 @@ export function convertFeaturesGeometryToGeometryTypeOfLayer({
  geometryType,
 }) {
  const converted = [];
+ const is3DGeometry = g3wsdk.core.geoutils.Geometry.is3DGeometry(geometryType);
+
  features.forEach(f => {
    const type = f.getGeometry() && f.getGeometry().getType();
-   if (geometryType === type) {
-     converted.push(f);
-   } else if (
+   //in the case of geometry type and layer is not 3dGeometry, remove eventually 3d coordinate
+   if (type && !is3DGeometry) {
+    g3wsdk.core.geoutils.Geometry.removeZValueToOLFeatureGeometry({ feature: f });
+   }
+   //in the case of geometry type and layer is not 3dGeometry, add 3d coordinate
+    if (type && !is3DGeometry) {
+     g3wsdk.core.geoutils.Geometry.addZValueToOLFeatureGeometry({ feature: f, geometryType });
+   }
+   if (geometryType === type) { converted.push(f) }
+   else if (
     g3wsdk.core.geoutils.isSameBaseGeometryType(type, geometryType) &&
      (g3wsdk.core.geometry.Geometry.isMultiGeometry(geometryType) || !g3wsdk.core.geometry.Geometry.isMultiGeometry(type))
    ) {
@@ -26,4 +35,4 @@ export function convertFeaturesGeometryToGeometryTypeOfLayer({
    }
  });
  return converted;
-};
+}
