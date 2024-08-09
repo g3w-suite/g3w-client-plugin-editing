3882a8260c6f0b25b7ef63710a9d5ea1144a62f5
volterra79 <boccacci.francesco@gmail.com>
2024-08-09 10:26:02 +0200

    Clean code - space

--- utils/getEditingLayerById.js
+++ utils/getEditingLayerById.js
@@ -3,6 +3,6 @@
  * 
  * @since g3w-client-plugin-editing@v3.8.0
  */
-export function getEditingLayerById(layerId){
+export function getEditingLayerById(layerId) {
   return g3wsdk.core.plugin.PluginsRegistry.getPlugin('editing').getLayerById(layerId);
 }
