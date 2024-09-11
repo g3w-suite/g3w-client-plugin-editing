ddbd54fba1ec7894cce0f08fd16675b241faad9e
volterra79 <boccacci.francesco@gmail.com>
2024-09-11 09:03:02 +0200

    Comments and push Promise to await purpose

--- utils/setLayerUniqueFieldValues.js
+++ utils/setLayerUniqueFieldValues.js
@@ -2,6 +2,10 @@ const { CatalogLayersStoresRegistry } = g3wsdk.core.catalog;
 
 /**
  * ORIGINAL SOURCE: g3w-client-plugin-editing/services/editingservice.js@v3.7.8
+ * Method to get unique values of unique input values from server
+ * It's called
+ * - When toolbox start
+ * - Open a relation from OpenFormStep
  * 
  * @param { string } layerId
  *
@@ -19,16 +23,20 @@ export async function setLayerUniqueFieldValues(layerId) {
     //filter field that is unique and not yet set unique values
     .filter(field => field.validate.unique && undefined === (service.state.uniqueFieldsValues[layerId] && service.state.uniqueFieldsValues[layerId][field.name]))
     .forEach((field => {
-      promises.push(
+      promises.push(new Promise((resolve, reject) => {
         layer
           .getFilterData({ unique: field.name })
           .then((values = []) => {
+            //check if not yet create
             if (undefined === service.state.uniqueFieldsValues[layerId]) {
               service.state.uniqueFieldsValues[layerId] = {};
             }
+            //set unique values for the field
             service.state.uniqueFieldsValues[layerId][field.name] = new Set(values);
+            resolve();
           })
-      );
+          .fail(e => { console.warn(e); reject(e); })
+      }))
     }))
   await Promise.allSettled(promises);
   return service.state.uniqueFieldsValues[layerId];
