4af7014a440a7afe2f5fa11ba0f711e3c6128abe
Raruto <Raruto@users.noreply.github.com>
2025-07-21 09:36:56 +0200

    Delete `ChooseFeatureToEdit.vue`

--- utils/chooseFeatureFromFeatures.js
+++ utils/chooseFeatureFromFeatures.js
@@ -1,4 +1,5 @@
-const _ = g3wsdk.core.i18n.t;
+const { GUI } = g3wsdk.gui;
+const _       = g3wsdk.core.i18n.t;
 
 /**
  * ORIGINAL SOURCE: g3w-client-plugin-editing/workflows/tasks/editingtask.js@v3.7.1
@@ -14,7 +15,57 @@ export function chooseFeatureFromFeatures({
 
     const feature = [];
 
-    const comp = new (Vue.extend(require('../components/ChooseFeatureToEdit.vue').default))({
+    const comp = new (Vue.extend({
+      template: /* html */`
+        <div id = "editing-layers-choose-feature">
+          <div
+            v-for = "(feature, index) in $options.features"
+            style = "align-items: center; padding: 5px; position: relative; display: flex; justify-content: space-between; border-bottom: 1px solid #eee;"
+          >
+
+            <section style = "display: flex; flex-direction: column; justify-content: space-between;">
+
+              <!-- CHOOSE FEATURE -->
+              <div>
+                <input
+                  :id         = "'choose_feature_'+ index"
+                  @click.stop = "selectFeature(feature)"
+                  name        = "radio"
+                  type        = "radio"
+                >
+                <label :for = "'choose_feature_' + index" style="color: transparent;">id</label>
+              </div>
+
+              <!-- ZOOM TO FEATURE -->
+              <div
+                @click.stop = "zoomToFeature(feature)"
+                :class      = "g3wtemplate.font['marker']"
+                class       = "skin-color"
+                style       = "padding-left: 3px; font-size: 1.3em; cursor: pointer; margin-top: 10px;"
+              ></div>
+
+            </section>
+
+            <!-- FEATURE ATTRIBUTES -->
+            <section style = "overflow-x: auto; display: flex">
+              <div v-for = "({ attribute, value }) in getAttributesFeature(feature)" style = "display: flex; flex-direction: column; justify-content: space-between; padding: 5px;">
+                <span style = "font-weight: bold; margin-bottom: 10px;">{{ attribute }}</span>
+                <span style = "align-self: start; white-space: nowrap;">{{ value }}</span>
+              </div>
+            </section>
+
+          </div>
+        </div>
+      `,
+      name: 'choosefeature',
+      data: () =>({ feature: this.$options.feature }),
+      methods: {
+        selectFeature(feature)        { this.feature.splice(0); this.feature.push(feature); },
+        getAttributesFeature(feature) { const props = feature.getProperties(); return this.$options.attributes.map(({ label, name }) => ({ attribute: label, value: props[name] })); },
+        zoomToFeature(feature)        { GUI.getService('map').zoomToFeatures([feature], { highlight: true, duration: 1000 }); },
+      },
+      mounted() { GUI.closeContent(); },
+    }))({
       features:   Array.isArray(features) ? features : [],
       feature,
       attributes: inputs.layer.getEditingFields().map(({ name, label }) => ({ name, label })),
