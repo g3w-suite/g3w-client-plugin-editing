71b07e283f41c5a14f014781cca974bd315c15a8
Raruto <Raruto@users.noreply.github.com>
2025-07-21 10:02:26 +0200

    Delete `Usermessage.vue`

--- g3w-workflow.js
+++ g3w-workflow.js
@@ -379,10 +379,56 @@ export class Workflow extends G3WObject {
           closable: false,
           hooks: {
             body: {
-              ...require('./components/UserMessage.vue').default,
-              data: () => ({
-                steps: this._userMessageSteps,
-              })
+              template: /* html */`
+              <ul class = "steps-list">
+                <li
+                  v-for  = "(step, id) in steps"
+                  :key   = "id"
+                  :style = "{ display: step.buttonnext && 'inline-flex' }"
+                  :class = "{ 'done': step.done }"
+                >
+                  <span v-if = "step.buttonnext" class="button-step">
+                    <span
+                      v-t-plugin = "step.description"
+                      class      = "description"
+                    ></span>
+                    <span
+                      v-if  = "step.dynamic"
+                      class = "dynamic-step"
+                    >{{ step.dynamic }}</span>
+                    <button
+                      @click = "completeStep(step)"
+                      :class = "'btn btn-success' + (step.buttonnext.disabled ? ' g3w-disabled' : '' )"
+                      style  = "margin-left: 10px;"
+                      v-t    = "'plugins.editing.workflow.next'"
+                    ></button>
+                  </span>
+                  <template v-else>
+                    <i :class = "g3wtemplate.getFontClass(step.done ? 'success' : 'empty-circle')"></i>
+                    <span v-t-plugin = "step.description"></span>
+                  </template>
+                </li>
+              </ul>
+              `,
+              data: () => ({ steps: this._userMessageSteps }),
+              methods: {
+                completeStep(step) { step.done = true; step.buttonnext.done(); },
+              },
+              beforeMount() {
+                document.head.insertAdjacentHTML(
+                  'beforeend',
+                  `<style id ="editing-usermessage-css">
+                    .steps-list                                       { align-self: flex-start; list-style: none; padding: 10px; margin-bottom: 0; }
+                    .steps-list li                                    { margin-bottom: 5px; }
+                    .steps-list li.done                               { font-weight: bold; color: green; }
+                    .steps-list li.done > .description                { font-weight: bold; }
+                    .steps-list .dynamic-step                         { padding: 10px; font-size: 1.2em; }
+                    .steps-list .button-step                          { display: inline-flex; align-items: center; }
+                    .steps-list :is(.button-step, button.btn-success) { font-weight: bold; align-self: normal; }
+                  </style>`
+                );
+              },
+              beforeDestroy() { document.head.querySelector('#editing-usermessage-css').remove(); }
             }
           }
         });
